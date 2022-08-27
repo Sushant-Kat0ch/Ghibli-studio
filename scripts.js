2e4140d39f43df7f7ca40527e4c3a4b24a495f7b
Sushant-Kat0ch <112236132+Sushant-Kat0ch@users.noreply.github.com>
2022-08-27 13:00:02 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -58,7 +58,7 @@ const app = document.getElementById('root');
 
 const logo = document.createElement('img');
 
-logo.src = './images/logo.png';
+logo.src = './logo.png';
 
 const container = document.createElement('div');    
 container.setAttribute('class', 'container');
