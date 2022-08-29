a29c4c97ea6c5e2d6070336c521929a90f8574eb
Sushant-Kat0ch <sushantkatoch16@gmail.com>
2022-08-29 18:50:27 +0530

    image path

--- scripts.js
+++ scripts.js
@@ -58,7 +58,7 @@ const app = document.getElementById('root');
 
 const logo = document.createElement('img');
 
-logo.src = './images/logo.png';
+logo.src = 'https://raw.githubusercontent.com/Sushant-Kat0ch/Ghibli-studio/main/images/logo.png';
 
 const container = document.createElement('div');    
 container.setAttribute('class', 'container');
